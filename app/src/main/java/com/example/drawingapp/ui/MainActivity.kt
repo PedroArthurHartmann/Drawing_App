496a2fda48936c818d6ed0701813bc0cb3b1c89e
PedroArthurHartmann <pedroaphartmann@gmail.com>
2023-01-22 19:04:55 -0300

    added request for storage access

--- app/src/main/java/com/example/drawingapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/ui/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.drawingapp.ui
 
+import android.Manifest
 import android.app.Dialog
+import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import com.example.drawingapp.R
@@ -15,6 +22,18 @@ class MainActivity : AppCompatActivity() {
     private lateinit var drawingView: DrawingView
     private lateinit var mImageButtonCurrentPaint: ImageButton
 
+    private val mGalleryAccess: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach {
+                val permissionName = it.key
+                val isGranted = it.value
+
+                if (isGranted) {
+
+                }
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,12 +42,25 @@ class MainActivity : AppCompatActivity() {
         findViewById<ImageButton>(R.id.ib_brush).setOnClickListener {
             showBrushSizeDialog()
         }
+        findViewById<ImageButton>(R.id.ib_gallery).setOnClickListener {
+            requestStoragePermission()
+        }
 
         mImageButtonCurrentPaint =
             findViewById<LinearLayout>(R.id.ll_paint_colors)[0] as ImageButton
         mImageButtonCurrentPaint.setImageDrawable(
             ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
         )
+    }
+
+    private fun requestStoragePermission() {
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        ) {
+            mGalleryAccess.launch(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE))
+        }
     }
 
     private fun showBrushSizeDialog() {
